032e7c8bfde82d1d8fce767fc6b6be3ec529eb36
Aiswarya S <134680109+aishushan@users.noreply.github.com>
2023-12-26 13:36:39 +0530

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -1,48 +1,44 @@
-import streamlit as st 
-from functions import extract_text_from_pdf,extract_keywords_from_text,clean_keywords,extract_text_from_doc,extract_text_from_image
+# extraction.py
+import streamlit as st
+from loaded_functions import loaded_extract_text_from_pdf, loaded_extract_keywords_from_text, loaded_clean_keywords, loaded_extract_text_from_doc, loaded_extract_text_from_image
 
-
-  
 st.title('**********KEYWORD EXTRACTION APPLICATION***********')
 
-#uploading the file 
-uploaded_file=st.file_uploader("choose a file", type=["pdf","docx","jpeg","png","jpg"])
+# Uploading the file
+uploaded_file = st.file_uploader("Choose a file", type=["pdf", "docx", "jpeg", "png", "jpg"])
 
 if uploaded_file is not None:
-    file_type=uploaded_file.type.split('/')[1]
-    
+    file_type = uploaded_file.type.split('/')[1]
+
     if file_type == "pdf":
-        pdf_text=loaded_extract_text_from_pdf(uploaded_file)
-        pdf_keywords=loaded_extract_keywords_from_text(pdf_text)
-        pdf_cleaned_keywords=loaded_clean_keywords(pdf_keywords)
-        
+        pdf_text = loaded_extract_text_from_pdf(uploaded_file)
+        pdf_keywords = loaded_extract_keywords_from_text(pdf_text)
+        pdf_cleaned_keywords = loaded_clean_keywords(pdf_keywords)
+
         st.header("PDF TEXT")
         st.text(pdf_text)
-        
+
         st.header("CLEANED KEYWORDS")
         st.write(pdf_cleaned_keywords)
-    
-    elif file_type=="docx":
-        doc_text=loaded_extract_text_from_doc(uploaded_file)
-        doc_keywords=loaded_extract_keywords_from_text(doc_text)
-        doc_cleaned_keywords=loaded_clean_keywords(doc_keywords)
-        
+
+    elif file_type == "docx":
+        doc_text = loaded_extract_text_from_doc(uploaded_file)
+        doc_keywords = loaded_extract_keywords_from_text(doc_text)
+        doc_cleaned_keywords = loaded_clean_keywords(doc_keywords)
+
         st.header("WORD TEXT")
         st.text(doc_text)
-        
+
         st.header("CLEANED KEYWORDS")
         st.write(doc_cleaned_keywords)
-        
-    elif file_type in ["jpeg","png","jpg"]:
-        image= Image.open(uploaded_file)
-        reader = easyocr.Reader(['en'])
-        result = reader.readtext(image)
-        image_text = ' '.join([entry[1] for entry in result])
+
+    elif file_type in ["jpeg", "png", "jpg"]:
+        image_text = loaded_extract_text_from_image(uploaded_file)
         image_keywords = loaded_extract_keywords_from_text(image_text)
         image_cleaned_keywords = loaded_clean_keywords(image_keywords)
-        
+
         st.header("IMAGE TEXT")
         st.text(image_text)
-        
+
         st.header("CLEANED KEYWORDS")
         st.write(image_cleaned_keywords)
