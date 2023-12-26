92b18c528931a691500f234f85bfa0a7531d1be9
Aiswarya S <134680109+aishushan@users.noreply.github.com>
2023-12-26 13:46:46 +0530

    Update loaded_functions.py

--- loaded_functions.py
+++ loaded_functions.py
@@ -1,10 +1,10 @@
 # loaded_functions.py
 import pickle
-import functions  # Import the functions module
+from functions import extract_text_from_pdf, extract_keywords_from_text, clean_keywords, extract_text_from_doc, extract_text_from_image
 
 with open('keyword_extraction.pkl', 'rb') as file:
-    loaded_extract_text_from_pdf = functions.extract_text_from_pdf
-    loaded_extract_keywords_from_text = functions.extract_keywords_from_text
-    loaded_clean_keywords = functions.clean_keywords
-    loaded_extract_text_from_doc = functions.extract_text_from_doc
-    loaded_extract_text_from_image = functions.extract_text_from_image
+    loaded_extract_text_from_pdf = extract_text_from_pdf
+    loaded_extract_keywords_from_text = extract_keywords_from_text
+    loaded_clean_keywords = clean_keywords
+    loaded_extract_text_from_doc = extract_text_from_doc
+    loaded_extract_text_from_image = extract_text_from_image
