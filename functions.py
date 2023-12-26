2f7c7e24f843aa176254937f3aa0b0db26c17f7e
Aiswarya S <134680109+aishushan@users.noreply.github.com>
2023-12-26 13:48:10 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
+# functions.py
 import pickle
 from docx import Document
 import fitz
+import nltk 
 import re
 import easyocr
 from rake_nltk import Rake
@@ -12,6 +14,7 @@ def extract_text_from_pdf(pdf_path):
     for page_num in range(doc.page_count):
         page = doc[page_num]
         text += page.get_text("text.utf8").replace('\uFFFD', 't')
+    print("Extracted Text from PDF:", text)
     return text
 
 # Function to extract keywords from text
@@ -19,11 +22,13 @@ def extract_keywords_from_text(text):
     r = Rake()
     r.extract_keywords_from_text(text)
     keywords = r.get_ranked_phrases_with_scores()
+    print("Extracted Keywords:", keywords)
     return keywords
 
 # Function to clean keywords
 def clean_keywords(keywords):
     cleaned_keywords = [(score, re.sub(r'[^a-zA-Z0-9\s]', '', phrase)) for score, phrase in keywords]
+    print("Cleaned Keywords:", cleaned_keywords)
     return cleaned_keywords
 
 # Function to extract text from Word document
@@ -32,6 +37,7 @@ def extract_text_from_doc(doc_path):
     text = ""
     for paragraph in doc.paragraphs:
         text += paragraph.text + "\n"
+    print("Extracted Text from DOCX:", text)
     return text
 
 # Function to extract text from image
@@ -39,6 +45,5 @@ def extract_text_from_image(image_path):
     reader = easyocr.Reader(['en'])
     result = reader.readtext(image_path)
     text = ' '.join([entry[1] for entry in result])
+    print("Extracted Text from Image:", text)
     return text
-
-
